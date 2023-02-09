0770ccbfad1226c4a9e11469f34c43f3d1f7fa11
Дмитрий <semenovsdo@mail.ru>
2023-02-09 17:05:57 +0300

    Seminar003

---
+++ Seminar003/Program.cs
@@ -0,0 +1,26 @@
+﻿Task1();
+void Task1()
+{
+    int x = ReadInt ("x");
+    int y = ReadInt ("y");
+    int quarter = GetQuarter (x, y);
+    Console.WriteLine($"{quarter}: [{x} {y}]");
+}
+int GetQuarter (int x, int y)
+{
+    if (x == 0 || y == 0) return -1;
+    if (x > 1 && y > 1) return 1;
+    if (x < -1 && y > 1) return 2;
+    if (x < -1 && y < -1) return 3;
+    else return 4;
+}
+int ReadInt (string argumentName)
+{
+    Console.WriteLine($"Input {argumentName}");
+    int number = 0;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not a number");
+    }
+    return number;
+}
